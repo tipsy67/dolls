0153aa0c3837607c9808cadb6f6e17589977c92e
dan.toropov <dan.toropov@gmail.com>
2025-02-10 20:50:53 +0300

    install django

---
+++ dolls/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DollsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dolls'
---
+++ users/models.py
@@ -0,0 +1,34 @@
+import random
+import string
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from config.settings import NULLABLE
+
+
+class User(AbstractUser):
+    tg_chat_id = models.CharField(
+        max_length=50, **NULLABLE, verbose_name="телеграм chat id"
+    )
+    avatar = models.ImageField(upload_to='users/', **NULLABLE, verbose_name='аватар')
+    phone = models.CharField(max_length=30, verbose_name='телефон')
+    token = models.CharField(max_length=100, **NULLABLE, verbose_name='токен')
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name[:1]}."
+
+    @property
+    def fio(self):
+        return f'{self.last_name} {self.first_name}'
+
+    @staticmethod
+    def generate_password(length: int):
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for _ in range(length))
+
+        return password
