554eb259bb05a13615fbd032fa931e3ca791f72b
dan.toropov <dan.toropov@gmail.com>
2025-02-11 22:35:56 +0300

    add templates

--- dolls/models.py
+++ dolls/models.py
@@ -1,3 +1,49 @@
+from config.settings import NULLABLE
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    is_published = models.BooleanField(default=False, verbose_name='Активно')
+    active_on_main_page = models.BooleanField(
+        default=False, verbose_name='Активно на гл.стр.'
+    )
+
+    ordering = ['name']
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    price = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='Цена')
+    image = models.ImageField(
+        upload_to='products/', **NULLABLE, verbose_name='Изображение'
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Изменен')
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.PROTECT,
+        related_name='products',
+        verbose_name='Категория',
+    )
+    parameter = models.CharField(max_length=30, verbose_name='Параметр', **NULLABLE)
+    is_published = models.BooleanField(default=False, verbose_name='Активно')
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name']
+
+    def __str__(self):
+        return f"{self.name}"
+
+
---
+++ dolls/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from dolls.apps import DollsConfig
+from dolls.views import main_page
+
+app_name = DollsConfig.name
+
+urlpatterns = [
+    path('', main_page, name='home'),
+    # path('contact/', FeedbackCreateView.as_view(), name='contact'),
+    # path('thank-you/', thank_you, name='thank-you'),
+    # path('about/', about, name='about'),
+    # path('shop-list/', product_list_view, name='shop-list'),
+    # path('service-list/', service_list_view, name='service-list'),
+    # path('service-detail/<int:pk>/', service_detail_view, name='service-detail'),
+]
