e33faf6e2d34753ed17d41eac0118b123b73bc0c
dan.toropov <dan.toropov@gmail.com>
2025-02-13 00:36:59 +0300

    edit templates ..

--- dolls/admin.py
+++ dolls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from dolls.models import Category, Product, Image
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
--- tunes/admin.py
+++ tunes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from tunes.models import Feedback, Quote, TunesDict, Banner, Contact
+from tunes.models import Feedback, TunesDict, Banner, Contact
 
 
 @admin.register(Feedback)
@@ -10,10 +10,6 @@ class FeedbackAdmin(admin.ModelAdmin):
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('email', 'phone', 'address',)
-
-@admin.register(Quote)
-class QuoteAdmin(admin.ModelAdmin):
-    pass
 
 @admin.register(TunesDict)
 class TunesDictAdmin(admin.ModelAdmin):
