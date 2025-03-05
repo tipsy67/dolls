7038298224420c4c1960151c03e0cff36acf9db2
dan.toropov <dan.toropov@gmail.com>
2025-03-05 21:55:23 +0300

    create orders models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+from config.settings import NULLABLE
+from dolls.models import Product
+
+
+class Order(models.Model):
+    STATUS_ORDER = {
+        'CREATED': "Создан",
+        'PAID': "Оплачен",
+        'SEND': "Отправлен",
+        'SHIPPED': "Доставлен",
+    }
+
+    country = models.CharField(max_length=50, verbose_name='Страна')
+    zip = models.CharField(max_length=20, verbose_name='Индекс')
+    address = models.CharField(max_length=255, verbose_name='Адрес')
+    comment = models.CharField(max_length=255, verbose_name='Комментарий', **NULLABLE)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        to='users.User', on_delete=models.CASCADE, related_name='orders'
+    )
+    status = models.CharField(max_length=10, choices=STATUS_ORDER, default='CREATED')
+
+    class Meta:
+        # db_table = 'optics_contact'
+        ordering = ['-created_at']
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
+
+    def __str__(self):
+        return f'Заказ {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
