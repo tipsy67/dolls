d5093245ac9d1a3c85fcaa77a46a7f19c47b9c8f
dan.toropov <dan.toropov@gmail.com>
2025-03-07 23:48:10 +0300

    begin blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.template.defaultfilters import truncatechars
+from pytils.translit import slugify
 
-# Create your models here.
+from users.models import NULLABLE, User
+from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Comment(MPTTModel):
+    article = models.ForeignKey(
+        'BlogArticle',
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Статья"
+    )
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True, blank=True,
+        related_name="children",
+        verbose_name="Родитель"
+    )
+    owner = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="Владелец"
+    )
+    text = models.CharField(max_length=1000, verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создан")
+
+    class MPTTMeta:
+        order_insertion_by = ['created_at']
+
+    def __str__(self):
+        return f"{self.owner}: {self.text[:20]}"
+
+
+class BlogArticle(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.CharField(max_length=100, unique=True, verbose_name='Slug')
+    image = models.ImageField(
+        upload_to='blog/', blank=True, verbose_name='Изображение'
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Изменен')
+    is_published = models.BooleanField(default=False, verbose_name='Признак публикации')
+    views_counter = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    like_counter = models.IntegerField(default=0, verbose_name='Количество лайков')
+    content = models.TextField(verbose_name='Содержимое')
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        related_name='blogs',
+        verbose_name='Владелец',
+    )
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.title}'
+
+    @property
+    def short_content(self):
+        return truncatechars(self.content, 100)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(self.title)
+        super(BlogArticle,self).save(*args, **kwargs)
--- dolls/views.py
+++ dolls/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
+from blog.models import BlogArticle
 from config.settings import PRODUCT_PER_PAGE
 from dolls.models import Category, Product
 from dolls.src.utils import get_random_reviews, get_queryset_from_cache
@@ -12,18 +13,23 @@ from tunes.models import Banner
 def main_page(request):
 
     banner_list = Banner.objects.filter(is_published=True)
-
     category_list = Category.objects.filter(is_published=True)
-
     product_list = Product.objects.filter(is_published=True).order_by('?')
+    blog_list = BlogArticle.objects.filter(is_published=True).order_by('?')
 
-    # blog_set = Blog.objects.filter(is_published=True).order_by('?')
-    # blog_list = blog_set[:3]
+
+    paginator_product = Paginator(product_list, PRODUCT_PER_PAGE)
+    page_num_product = request.GET.get('page_prod', 1)
+    page_product = paginator_product.get_page(page_num_product)
+
+    paginator_blog = Paginator(blog_list, 3)
+    page_num_blog = request.GET.get('page_blog', 1)
+    page_blog = paginator_blog.get_page(page_num_blog)
 
     context = {
-        'product_list': product_list,
+        'product_list': page_product,
         'category_list': category_list,
-        # 'blog_list': blog_list,
+        'blog_list': page_blog,
         'testimonial_list': get_random_reviews(request),
         'banner_list': banner_list,
         'home': 'active',
@@ -50,15 +56,14 @@ def product_list_view(request, cat=None):
         product_list = product_list.filter(tags__pk__in=tags.tags)
 
     paginator = Paginator(product_list, PRODUCT_PER_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_num_product = request.GET.get('page', 1)
+    page_product = paginator.get_page(page_num_product)
 
     context = {
-        'product_list': page_obj,
+        'product_list': page_product,
         'category_list': category_list,
         'testimonials': get_random_reviews(request),
         'shop': 'active',
-        'page_number': page_number,
         'category_pk': cat,
         'sale_list': sale_list,
         'tag_list': tag_list,
