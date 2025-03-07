2b7d1d53f1281116220a59c3a41cb0c5f0dc8cb1
dan.toropov <dan.toropov@gmail.com>
2025-03-07 23:48:10 +0300

    begin blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from blog.models import BlogArticle
+from config.settings import BLOG_PER_PAGE
+
+
+class BlogListView(ListView):
+    model = BlogArticle
+    paginate_by = BLOG_PER_PAGE
+
+    extra_context = {
+    }
+
+    def get_queryset(self):
+        return BlogArticle.objects.filter(is_published=True)
+
+
+class BlogDetailView(DetailView):
+    model = BlogArticle
+    extra_context = {
+    }
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        # self.streamer_path = self.streamer_path[:3]
+        # self.streamer_path.append({'name' : self.object.title[:12]+'...', })
+        self.object.save()
+
+        return self.object
