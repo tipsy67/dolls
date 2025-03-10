fb2f9a38caa1f5a0592c81994b7d376dd48c9b50
dan.toropov <dan.toropov@gmail.com>
2025-03-10 12:50:12 +0300

    add js like

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from blog.apps import BlogConfig
-from blog.views import BlogDetailView, BlogListView
+from blog.views import BlogDetailView, BlogListView, article_like
 
 appname = BlogConfig.name
 
 urlpatterns = [
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog-detail/<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('like/', article_like, name='like'),
 ]
