0b0daee608c9242648e2a59c713a4ca521966b16
dan.toropov <dan.toropov@gmail.com>
2025-03-16 00:19:23 +0300

    begin add new templates

--- dolls/views.py
+++ dolls/views.py
@@ -1,8 +1,10 @@
+from django.db.models.functions import Random
+
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from blog.models import BlogArticle
-from config.settings import PRODUCT_PER_PAGE
+from config.settings import PRODUCT_PER_PAGE, BLOG_PER_PAGE
 from dolls.models import Category, Product
 from dolls.src.utils import get_random_reviews, get_queryset_from_cache
 from tags.models import Tag
@@ -14,22 +16,20 @@ def main_page(request):
 
     banner_list = Banner.objects.filter(is_published=True)
     category_list = Category.objects.filter(is_published=True)
-    product_list = Product.objects.filter(is_published=True).order_by('?')
-    blog_list = BlogArticle.objects.filter(is_published=True).order_by('?')
+    objects_list=[]
+    for category in category_list:
+        product_list = Product.objects.filter(is_published=True, category=category).order_by(Random())[:PRODUCT_PER_PAGE]
+        objects_list.append({
+            'name': category.name,
+            'product_list': product_list
+        })
+    blog_list = BlogArticle.objects.filter(is_published=True).order_by(Random())[:BLOG_PER_PAGE]
 
 
-    paginator_product = Paginator(product_list, PRODUCT_PER_PAGE)
-    page_num_product = request.GET.get('page_prod', 1)
-    page_product = paginator_product.get_page(page_num_product)
-
-    paginator_blog = Paginator(blog_list, 3)
-    page_num_blog = request.GET.get('page_blog', 1)
-    page_blog = paginator_blog.get_page(page_num_blog)
 
     context = {
-        'product_list': page_product,
-        'category_list': category_list,
-        'blog_list': page_blog,
+        'objects_list': objects_list,
+        'blog_list': blog_list,
         'testimonial_list': get_random_reviews(request),
         'banner_list': banner_list,
         'home': 'active',
