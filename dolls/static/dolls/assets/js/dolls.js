9b2c6e7b36e37490e47d54452abf5e18c077feaf
dan.toropov <dan.toropov@gmail.com>
2025-04-02 22:59:06 +0300

    feature: remake buttons fixed: templates

--- dolls/static/dolls/assets/js/dolls.js
+++ dolls/static/dolls/assets/js/dolls.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    restoreScrollPosition();
+//    restoreScrollPosition();
     makeTableRowsClickable();
     setupLikeHandlers();
     setupChangeStatusHandler();
