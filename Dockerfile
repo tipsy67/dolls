84cdde1d90cf60cb5400c3cb89b26f66106da413
dan.toropov <dan.toropov@gmail.com>
2025-04-22 23:46:53 +0300

    feature: created docker .

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN pip install --no-cache-dir --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
-#CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
+#RUN chmod +x /app/entrypoint.sh
+#ENTRYPOINT ["/app/entrypoint.sh"]
 
 
---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+  echo "Running migrations..."
+  python manage.py migrate
+
+  echo "Collecting static files..."
+  python manage.py collectstatic --noinput
+
+  echo "create custom superuser..."
+  python manage.py csu
+
+  echo "Load json data..."
+  python manage.py loaddata data.json
+
+  echo "Load gunicorn..."
+  gunicorn config.wsgi:application --bind 0.0.0.0:8000
