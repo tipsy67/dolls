6e05593057207f09336b0074fe912f15b89a4bfa
dan.toropov <dan.toropov@gmail.com>
2025-05-10 20:36:08 +0300

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@
   echo "Collecting static files..."
   python manage.py collectstatic --noinput
 
-  echo "create custom superuser..."
-  python manage.py csu
-
-  echo "Load json data..."
-  python manage.py loaddata data.json
+#  echo "create custom superuser..."
+#  python manage.py csu
+#
+#  echo "Load json data..."
+#  python manage.py loaddata data.json
 
   echo "Load gunicorn..."
   gunicorn config.wsgi:application --bind 0.0.0.0:8000
