42c1a71ceceee955e32c523a4024c013a32cbfcf
dan.toropov <dan.toropov@gmail.com>
2025-05-10 20:12:05 +0300

    fix: social.html

--- tunes/context_processors.py
+++ tunes/context_processors.py
@@ -7,5 +7,9 @@ def get_tunes(request):
     context['author_email'] = get_value_from_tunes('author_email')
     context['limit_free_shipping'] = get_value_from_tunes('limit_free_shipping')
     context['insta_name'] = get_value_from_tunes('insta_name')
+    context['url_instagram'] = get_value_from_tunes('url_instagram')
+    context['url_pinterest'] = get_value_from_tunes('url_pinterest')
+    context['url_youtube'] = get_value_from_tunes('url_youtube')
+    context['url_vk'] = get_value_from_tunes('url_vk')
 
     return context
